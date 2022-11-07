d9bf299daa2dd77297517f0ba88d732499abb4d1
Mike Webb <mikewebb@live.com.au>
2022-11-07 21:07:07 +0800

    added to keymaps

--- lua/local/core/keymaps.lua
+++ lua/local/core/keymaps.lua
@@ -12,16 +12,29 @@ vim.g.mapleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
+-----------------------
 -- general keymaps
+-----------------------
 
 key.set("i", "jk", "<ESC>")   -- chord jk keys instead of using esc key to exit insert mode
-
+key.set("v", "jk", "<ESC>")   -- chord jk keys instead of using esc key to exit visual mode
 key.set("n", "x", '"_x')  -- stop a deleted characture from going into paste buffer
-
 key.set("n", "<leader>nh", ":nohl<CR>")  -- clear highlighted search hit
 
+-- window management
+key.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+key.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+key.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+key.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
- 
+key.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+key.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+key.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+key.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
+key.set("v", "<Tab>", ">gv") -- Tab to increase indent of visual selection
+key.set("v", "<S-Tab>", "<gv") -- Shift-tab to decrease indent of visual selelction
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
