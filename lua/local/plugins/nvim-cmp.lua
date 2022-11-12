398020ca09436a7943dc71faad083cfa4e0d2437
Mike Webb <mikewebb@live.com.au>
2022-11-12 13:35:09 +0800

    disable nvim-cmp function in comments

--- lua/local/plugins/nvim-cmp.lua
+++ lua/local/plugins/nvim-cmp.lua
@@ -8,6 +8,10 @@ if not snip_status_ok then
   return
 end
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 require("luasnip/loaders/from_vscode").lazy_load()
 
 local check_backspace = function()
@@ -46,6 +50,17 @@ local kind_icons = {
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
 cmp.setup {
+  enabled = function()
+    -- disable completion in comments
+    local context = require 'cmp.config.context'
+    -- keep command mode completion enabled when cursor is in a comment
+    if vim.api.nvim_get_mode().mode == 'c' then
+      return true
+    else
+      return not context.in_treesitter_capture("comment") 
+        and not context.in_syntax_group("Comment")
+    end
+  end,
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
@@ -81,6 +96,7 @@ cmp.setup {
       "i",
       "s",
     }),
+
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
@@ -94,6 +110,7 @@ cmp.setup {
       "s",
     }),
   },
+
   formatting = {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
